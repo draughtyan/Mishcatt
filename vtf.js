9b3e98a8346d11498700caa6084b16ed3e539f22
draughtyan <90784553+draughtyan@users.noreply.github.com>
2025-09-27 03:02:08 -0500

    remove filesize check

--- vtf.js
+++ vtf.js
@@ -98,17 +98,17 @@ function setResolution() {
 }
 
 function check() {
-	if (getEstFileSize(false)/1024 >= 512 && getEstFileSize(false)/1024 < 513){
+	if (false && (getEstFileSize(false)/1024 >= 512 && getEstFileSize(false)/1024 < 513)){
 		shortened = true;
 		document.getElementById("resolutionNotice").innerHTML = "Changed to "+(width-4)+"x"+height;
 		document.getElementById("resolutionNotice").style.visibility = "visible";
 	}
-	else if (shortened && getEstFileSize(false)/1024 < ((width - 4) / width) * 512 - 1){
+	else if (true || (shortened && getEstFileSize(false)/1024 < ((width - 4) / width) * 512 - 1)){
 		shortened = false;
 		document.getElementById("resolutionNotice").innerHTML = "";
 		document.getElementById("resolutionNotice").style.visibility = "hidden";
 	}
-	if (getEstFileSize(false)/1024 >= 385 || shortened || document.getElementById("sampling").value == 1 || width % 64 != 0 || height % 64 != 0){
+	if (false && (getEstFileSize(false)/1024 >= 385 || shortened || document.getElementById("sampling").value == 1 || width % 64 != 0 || height % 64 != 0)){
 		document.getElementById("mipmapsCheck").disabled = true;
 		document.getElementById("mipmapsCheck").checked = false;
 	}
@@ -117,7 +117,7 @@ function check() {
 			document.getElementById("mipmapsCheck").checked = true;
 		document.getElementById("mipmapsCheck").disabled = false;
 	}
-	reducedMipmaps = getEstFileSize(false)/1024 >= 384;
+	reducedMipmaps = false;
 	showMipmap(document.getElementById("mipmapsCheck"));
 }
 
@@ -1051,3 +1051,4 @@ function downloadVMT(){
 }`;
 	download(vmtFileText, "vmt");
 }
+
